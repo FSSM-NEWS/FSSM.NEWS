4eb1d9f2f5d55d25fdb4a8f1b2ec6d24ec8ca1ce
FSSM-NEWS <96381263+FSSM-NEWS@users.noreply.github.com>
2022-01-03 20:19:21 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,85 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m ;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+   
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+ 
+     $('html, body').animate({scrollTop:0}, 'slow');
+}
+
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+var user;
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    var cook2 = document.getElementById("cook2");
+    cook2.setAttribute("style", "display: block");
+    document.getElementById("user").innerHTML = user;
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  }
+}
+
+function modify() {
+    
+    var cook = document.getElementById("cook");
+            cook.setAttribute("style", "display: none");
+    var cook2 = document.getElementById("cook2");
+            cook2.setAttribute("style", "display: block");
+    document.getElementById("user").innerHTML =user;
+
+    
+    
+}
